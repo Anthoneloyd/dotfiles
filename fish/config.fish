bb286eb4d08771f4296fde316e95771780c80592
Andrés Garcés <agarcesb@unal.edu.co>
2024-06-01 22:18:24 -0500

    fish changes

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+function fish_greeting
+    set -l random_greeting "ola" 
+    echo "$random_greeting"
+end
+
 # Created by `pipx` on 2024-05-18 01:15:54
 set PATH $PATH /home/andres/.local/bin
