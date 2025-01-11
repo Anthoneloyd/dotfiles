d7ceea07e13a117be7b0fa8328b567c67a6533f7
Andrés Garcés <agarcesb@unal.edu.co>
2025-01-10 23:11:05 -0500

    aaaaa

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "rust_analyzer", "pylyzer", "clangd"}
+local servers = { "html", "cssls", "rust_analyzer", "ruff", "clangd"}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
